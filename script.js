0c8f5c10f376d58e09382f2778287ef3023e6938
matteo-didone <matteo.dido9@gmail.com>
2023-06-07 15:32:02 +0200

    Trying to add icons and profile pic

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ createApp({
                 {
                     // Contact Michele
                     name: 'Michele',
-                    avatar: './img/avatar_1.png',
+                    avatar: './img/avatar_1.jpg',
                     visible: true,
                     messages: [
                         {
@@ -55,7 +55,7 @@ createApp({
                 // Contact Fabio
                 {
                     name: 'Fabio',
-                    avatar: './img/avatar_2.png',
+                    avatar: './img/avatar_2.jpg',
                     visible: true,
                     messages: [
                         {
@@ -78,7 +78,7 @@ createApp({
                 // Contact Samuele
                 {
                     name: 'Samuele',
-                    avatar: './img/avatar_3.png',
+                    avatar: './img/avatar_3.jpg',
                     visible: true,
                     messages: [
                         {
@@ -101,7 +101,7 @@ createApp({
                 // Contact Alessandro B.
                 {
                     name: 'Alessandro B.',
-                    avatar: './img/avatar_4.png',
+                    avatar: './img/avatar_4.jpg',
                     visible: true,
                     messages: [
                         {
@@ -119,7 +119,7 @@ createApp({
                 // Contact Alessandro L.
                 {
                     name: 'Alessandro L.',
-                    avatar: './img/avatar_5.png',
+                    avatar: './img/avatar_5.jpg',
                     visible: true,
                     messages: [
                         {
@@ -137,7 +137,7 @@ createApp({
                 // Contact Claudia
                 {
                     name: 'Claudia',
-                    avatar: './img/avatar_5.png',
+                    avatar: './img/avatar_5.jpg',
                     visible: true,
                     messages: [
                         {
@@ -160,7 +160,7 @@ createApp({
                 // Contact Federico
                 {
                     name: 'Federico',
-                    avatar: './img/avatar_7.png',
+                    avatar: './img/avatar_7.jpg',
                     visible: true,
                     messages: [
                         {
@@ -178,7 +178,7 @@ createApp({
                 // Contact Davide
                 {
                     name: 'Davide',
-                    avatar: './img/avatar_8.png',
+                    avatar: './img/avatar_8.jpg',
                     visible: true,
                     messages: [
                         {
@@ -204,7 +204,7 @@ createApp({
 
     // Methods of the app
     methods: {
-        
+
     },
 
 }).mount('#app');
